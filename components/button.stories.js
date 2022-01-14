04ed859f27a88db5d58db43cf58400f75beb0bbb
Jeremias Erbs <jerry@badabam.de>
2022-01-14 18:22:39 +0100

    Add simple storybook example

---
+++ components/button.stories.js
@@ -0,0 +1,8 @@
+import './button.css';
+export default {
+  title: 'Components/button'
+}
+
+export const Default = () => '<button class="button">Click me</button>'
+export const Big = () => '<button class="button button--big">Big</button>'
+export const Inverted = () => '<button class="button button--inverted">Inverted</button>'
